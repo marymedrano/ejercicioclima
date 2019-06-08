4c32b602155f644069804ea1d7acd85af114a48b
Mary <mlmedrano@uamv.edu.ni>
2019-06-08 17:48:41 -0600

    project

---
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.prueba1;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView helloTextView = (TextView) findViewById(R.id.text);
+
+        //LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=12198b9facea441be4dde6b524c94b97";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Gson gson = new GsonBuilder().create();
+
+                        // Define Response class to correspond to the JSON response returned
+                        WeatherResponse weatherResponse = gson.fromJson(response, WeatherResponse.class);
+                        helloTextView.setText("Response is: " + weatherResponse.getMessage());
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                helloTextView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    class WeatherResponse {
+        private int cod;
+        private String message;
+        private int cnt;
+        private Location city;
+
+        public WeatherResponse() {
+        }
+
+        public int getCod() {
+            return cod;
+        }
+
+        public void setCod(int cod) {
+            this.cod = cod;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getCnt() {
+            return cnt;
+        }
+
+        public void setCnt(int cnt) {
+            this.cnt = cnt;
+        }
+
+        public Location getCity() {
+            return city;
+        }
+
+        public void setCity(Location city) {
+            this.city = city;
+        }
+    }
+
+    class Location {
+        private int id;
+
+        private String name;
+
+        private Coord coord;
+
+        private String country;
+
+        public Location() {
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Coord getCoord() {
+            return coord;
+        }
+
+        public void setCoord(Coord coord) {
+            this.coord = coord;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+    }
+
+    class Coord {
+        private long lat;
+
+        private long lon;
+
+        public Coord() {
+        }
+
+        public long getLat() {
+            return lat;
+        }
+
+        public void setLat(long lat) {
+            this.lat = lat;
+        }
+
+        public long getLon() {
+            return lon;
+        }
+
+        public void setLon(long lon) {
+            this.lon = lon;
+        }
+    }
+}
